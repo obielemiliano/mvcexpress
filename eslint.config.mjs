c53ca4fa0186526d17a84550e759b2a234d6eb66
obielemiliano <a01067876@tec.mx>
2024-10-08 17:06:33 -0600

    fit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
 
 export default [
   { ignores: ["dist/"] },
@@ -10,4 +10,5 @@ export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended
 ];
