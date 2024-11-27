5406ca14a07e852c51987a931a1d65667e9d2f60
obielemiliano <a01067876@tec.mx>
2024-11-26 19:57:49 -0600

    sensor id

--- src/interfaces/air.ts
+++ src/interfaces/air.ts
@@ -3,5 +3,4 @@ export interface Air {
   id?: number;
   co2_level: number; // Nivel de CO2 en partes por millón (ppm)
   timestamp: string; // Fecha y hora del registro
-  sensor_id?: number;
 }
--- src/interfaces/movement.ts
+++ src/interfaces/movement.ts
@@ -1,7 +1,6 @@
 // Tipo para representar un Movimiento
 export interface Movement {
   id?: number;
-  sensor_id?: number;
   timestamp: string; // Fecha y hora del registro
   detected: boolean; // Indica si se detectó movimiento
 }
